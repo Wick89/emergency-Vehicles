ba52504bb158b1162b44256cd7ae5659e70cf43b
Wick89 <44001562+Wick89@users.noreply.github.com>
2020-03-14 14:56:58 +0100

    updated v2.4

---
+++ ELS/vcf.lua
@@ -0,0 +1,13 @@
+vcf_files = {
+	"usIndsats.xml",
+	"FIRETRUK.xml",
+	"ENGINE9B.xml",
+	"ENGINE2.xml",
+	"ENGINE11.xml",
+	"ENGINE2A.xml",
+	"ENGINE2B.xml",
+	"RESCUE9A.xml",
+	"rescue2a.xml",
+	"Indsatsleder.xml",
+	"POLICET.xml"
+}
